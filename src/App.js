01e40b5f593d4aa185dce57ed560909856c9ba3d
moezzouari <moez@octoplus.tech>
2023-07-26 15:04:14 +0100

    Adding Scroll Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductApp from './components/articles/ProductApp'
 import './App.css';
-import ProductApp from './components/articles/ProductApp';
 import ProductsCard from './components/articles/ProductsCard';
+import Navscroll from './components/articles/Navscroll';
 
 function App() {
   return (
-    <div className="App">
-      <ProductsCard />
-    </div>
+    <Router>
+      <Navscroll />
+      <Routes>
+        <Route path='/produits' element={<ProductApp />} />
+        <Route path='/productcard' element={<ProductsCard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/articles/Itemcard.js
+++ src/components/articles/Itemcard.js
@@ -6,8 +6,8 @@ import React from 'react'
 const Itemcard = ({ pr }) => {
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={pr.imageart} />
+            <Card style={{ width: '20rem', margin: 2 }}>
+                <Card.Img variant="top" src={pr.imageart} width={100} height={320} />
                 <Card.Body>
                     <Card.Title>{pr.designation}</Card.Title>
                     <Card.Text>
